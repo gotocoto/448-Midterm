554cea742e9fcd8bb589615e65685e3e2d207ab3
Kyle Chen <kylechen@Kyles-MacBook-Air.local>
2023-09-26 00:26:30 -0400

    added basic log_regression

--- bayesian.py
+++ bayesian.py
@@ -1,6 +1,15 @@
+import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+
+# Open train file
+with open('train.txt', 'r') as file:
+    train_data = file.readlines()
+
+# open test file
+with open('test.txt', 'r') as file:
+    test_data = file.readlines()
 
 # Assume X, y are your data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
